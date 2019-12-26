df9c5dcd3f359fd001db68bea7812ef7fb24d7a7
Patrick Gallagher <patrickj@cpgallagher.com>
2019-12-26 01:05:05 -0600

    Restructure directories Bundles the solution with the rest of the project to avoid directory bloat

--- queueable-scoreboard-assistant/Properties/AssemblyInfo.cs
+++ queueable-scoreboard-assistant/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Queable Scoreboard Assistant")]
+[assembly: AssemblyTitle("queueable-scoreboard-assistant")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Queable Scoreboard Assistant")]
+[assembly: AssemblyProduct("queueable-scoreboard-assistant")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
