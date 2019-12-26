f7571d200a090376ea9fd66d473c0146fd7ecbf0
Patrick Gallagher <patrickj@cpgallagher.com>
2019-12-26 01:43:49 -0600

    Add a manager for the autocomplete model

--- queueable-scoreboard-assistant/Common/AutocompleteModelManager.cs
+++ queueable-scoreboard-assistant/Common/AutocompleteModelManager.cs
@@ -8,5 +8,34 @@ namespace queueable_scoreboard_assistant.Common
 {
     class AutocompleteModelManager
     {
+        private string _prefix;
+
+        /// <summary>
+        /// Adds a name not currently handled by the DFA to it.
+        /// </summary>
+        /// <param name="newName">the name to add to the DFA</param>
+        private void WriteNewName(string newName)
+        {
+
+        }
+
+        /// <summary>
+        /// Defines the response that the manager takes when a user selects a name.
+        /// </summary>
+        /// <param name="selectedName">the name that the user chose</param>
+        public void OnUserSelect(string selectedName) { 
+
+        }
+
+        /// <summary>
+        /// Adds a character to the prefix search. 
+        /// 
+        /// The maintained DFA requires us to keep track of the traversed path, which is done through a prefix.
+        /// </summary>
+        /// <param name="addition"></param>
+        public void AddPrefixChar(char addition)
+        {
+
+        }
     }
 }
