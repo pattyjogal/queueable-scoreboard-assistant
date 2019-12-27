e534767f01e7696eb1322978e8b2911463009749
Patrick Gallagher <patrickj@cpgallagher.com>
2019-12-26 21:03:53 -0600

    Add unit testing project

---
+++ queueable-scoreboard-assistant-test/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace queueable_scoreboard_assistant_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
