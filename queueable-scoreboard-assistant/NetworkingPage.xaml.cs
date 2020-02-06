f1a4bc14c0a61971d0d05961231c138d0d541cec
Patrick Gallagher <patrickj@cpgallagher.com>
2020-02-05 19:28:52 -0600

    Add hosting functionality

--- queueable-scoreboard-assistant/NetworkingPage.xaml.cs
+++ queueable-scoreboard-assistant/NetworkingPage.xaml.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Networking;
+using Windows.Networking.Connectivity;
+using Windows.Networking.Sockets;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,6 +30,51 @@ namespace queueable_scoreboard_assistant
             this.InitializeComponent();
         }
 
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
+
+        private void HostButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartServer();
+        }
+
+        private async void StartServer()
+        {
+            try
+            {
+                var streamSocketListener = new StreamSocketListener();
+                streamSocketListener.ConnectionReceived += StreamSocketListener_ConnectionReceived;
+
+                await streamSocketListener.BindServiceNameAsync(App.PortNumber);
+
+
+                foreach (HostName localHostName in NetworkInformation.GetHostNames())
+                {
+                    if (localHostName.IPInformation != null)
+                    {
+                        if (localHostName.Type == HostNameType.Ipv4)
+                        {
+                            IPAddrText.Text = localHostName.ToString();
+                            break;
+                        }
+                    }
+                }
+
+                OkStatusPanel.Visibility = Visibility.Visible;
+            }
+            catch (Exception ex)
+            {
+                SocketErrorStatus webErrorStatus = SocketError.GetStatus(ex.GetBaseException().HResult);
+                HostErrorMessage.Text = webErrorStatus.ToString() != "Unknown" ? webErrorStatus.ToString() : ex.Message;
+                BadStatusPanel.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void StreamSocketListener_ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
